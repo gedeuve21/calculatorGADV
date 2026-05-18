7977a5bb0cabb19caaedf2a76077678403014c01
Guillermo Álvarez de Ves <sologuille2014@gmail.com>
2026-05-18 13:56:02 +0200

    Se añaden suma, controlador y test unitario

---
+++ src/main/java/com/dam/calculatorGADV/Calculator.java
@@ -0,0 +1,10 @@
+package com.dam.calculatorGADV;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+    int sum(int a, int b) {
+        return a + b;
+    }
+}
