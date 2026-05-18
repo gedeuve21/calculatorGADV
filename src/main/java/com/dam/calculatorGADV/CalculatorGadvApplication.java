45b99925be335a3d82fb8c3c4a2fcb44aa7af8c6
Guillermo Álvarez de Ves <sologuille2014@gmail.com>
2026-05-18 13:11:49 +0200

    Añadido esqueleto Spring Boot

---
+++ src/main/java/com/dam/calculatorGADV/CalculatorGadvApplication.java
@@ -0,0 +1,13 @@
+package com.dam.calculatorGADV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorGadvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorGadvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dam/calculatorGADV/CalculatorGadvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam.calculatorGADV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorGadvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
